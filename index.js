61192d41f3f8d32136da83859bf85440dd40e369
Indiacash23 <sadsad11gg@gmail.com>
2025-05-08 19:51:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
+import express from "express";
+import axios from "axios";
+const PORT = process.env.PORT || 5500;
+const app = express();
+app.use(
+  express.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf;
+    }
+  })
+);
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://instacash-india.webflow.io");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+  });
+
+const token = '20c3c8ff3b9adbeeabb3730677330760459d9a62d396ec2377949491a2725c72';
+const collectionId = '681b28fcc8c82028a58b5955';
+
+app.post('/hello', (req, res) => {
+  res.send('Hello world');
+});
+
 const axios = require('axios');
 
 const token = '20c3c8ff3b9adbeeabb3730677330760459d9a62d396ec2377949491a2725c72';
 const collectionId = '681b28fcc8c82028a58b5955';
 
 app.post('/order', async (req, res) => {
-  const phoneFull = req.body.phone_full;
+  const formData = req.body;
+  const phoneFull = formData.phone_full;
 
   const options = {
     method: 'GET',
-    url: `https://api.webflow.com/v2/collections/${collectionId}/items?limit=100`,
+    url: https://api.webflow.com/v2/collections/${collectionId}/items?limit=100,
     headers: {
       accept: 'application/json',
-      authorization: `Bearer ${token}`,
+      authorization: Bearer ${token},
     },
   };
 
@@ -19,23 +48,34 @@ app.post('/order', async (req, res) => {
     const response = await axios.request(options);
     const items = response.data.items;
 
-    // Знайти елемент за номером телефону
     const foundItem = items.find(item => item.fieldData.name === phoneFull);
 
     if (foundItem) {
       return res.status(200).json({
         found: true,
         message: 'Елемент знайдено',
-        data: foundItem,
+        matchedItem: foundItem,
+        formData: formData,
       });
     } else {
       return res.status(200).json({
         found: false,
         message: 'Елемент з таким номером не знайдено',
+        formData: formData,
       });
     }
   } catch (error) {
     console.error('Помилка при зверненні до Webflow API:', error.response?.data || error.message);
-    return res.status(500).json({ error: 'Помилка сервера при пошуку номера' });
+    return res.status(500).json({
+      error: 'Помилка сервера при пошуку номера',
+      formData: formData,
+    });
   }
 });
+
+
+
+
+// ----------------
+
+app.listen(PORT, () => console.log("Server on " + PORT))
