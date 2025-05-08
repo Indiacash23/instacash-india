572ff562fce73f6e19a8c8edc8bf9611205f0b73
Indiacash23 <sadsad11gg@gmail.com>
2025-05-08 21:09:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -68,6 +68,13 @@ app.post('/order', async (req, res) => {
           fieldData: {
             name: phoneFull,
             slug: phoneFull.replace(/\+/g, ''),
+            "full-name": formData["First-Name"] + ' ' + formData["Last-Name"],
+            "city": formData["City"],
+            "language": formData["Language"],
+            "sum": formData["Sum"],
+            "the-request-has-been-processed": false,
+            "messenger": formData["Messenger"],
+            "status": "61da663c1046e1c2a962dd15679ce3b1",
           },
         },
       };
