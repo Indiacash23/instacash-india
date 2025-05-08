04d10046087a7506c032de7d6d2f227a546accd4
Indiacash23 <sadsad11gg@gmail.com>
2025-05-08 19:49:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 import express from "express";
-import cors from "cors";
 import axios from "axios";
-
 const PORT = process.env.PORT || 5500;
 const app = express();
-
-app.use(cors({
-  origin: 'https://instacash-india.webflow.io',
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
 app.use(
   express.json({
     verify: (req, res, buf) => {
@@ -18,6 +9,15 @@ app.use(
     }
   })
 );
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://instacash-india.webflow.io");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+  });
 
 const token = '20c3c8ff3b9adbeeabb3730677330760459d9a62d396ec2377949491a2725c72';
 const collectionId = '681b28fcc8c82028a58b5955';
@@ -26,16 +26,21 @@ app.post('/hello', (req, res) => {
   res.send('Hello world');
 });
 
+const axios = require('axios');
+
+const token = '20c3c8ff3b9adbeeabb3730677330760459d9a62d396ec2377949491a2725c72';
+const collectionId = '681b28fcc8c82028a58b5955';
+
 app.post('/order', async (req, res) => {
   const formData = req.body;
   const phoneFull = formData.phone_full;
 
   const options = {
     method: 'GET',
-    url: `https://api.webflow.com/v2/collections/${collectionId}/items?limit=100`,
+    url: https://api.webflow.com/v2/collections/${collectionId}/items?limit=100,
     headers: {
       accept: 'application/json',
-      authorization: `Bearer ${token}`,
+      authorization: Bearer ${token},
     },
   };
 
@@ -68,4 +73,9 @@ app.post('/order', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log("Server on " + PORT));
+
+
+
+// ----------------
+
+app.listen(PORT, () => console.log("Server on " + PORT))
