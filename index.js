ee4eebd9763026b60e389e87ffceae596b00d8d6
Indiacash23 <sadsad11gg@gmail.com>
2025-05-09 14:51:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,13 +40,11 @@ app.post('/order', async (req, res) => {
     const response = await axios.request(options);
     const items = response.data.items;
     const foundItem = items.find(item => item.fieldData.name.replace(/\s+/g, '') === phoneFull);
-
     if (foundItem) {
       const originalSum = parseFloat(foundItem.fieldData.sum || 0);
       const newSum = parseFloat(formData["Sum"] || 0);
       let currentSum = originalSum + newSum;
       if (currentSum > 150000) currentSum = 150000;
-
       const updateItemOptions = {
         method: 'PATCH',
         url: `https://api.webflow.com/v2/collections/${collectionId}/items/${foundItem.id}/live`,
@@ -65,14 +63,10 @@ app.post('/order', async (req, res) => {
           },
         },
       };
-
       const updateResponse = await axios.request(updateItemOptions);
-      console.log("Айтем оновлено:", updateResponse.data);
-      return res.status(200).json({
-        message: "Айтем оновлено успішно",
-        updatedSum: currentSum,
-      });
-
+      // return res.status(200).json({
+      //   updatedSum: currentSum,
+      // });
     } else {
       const createItemOptions = {
         method: 'POST',
@@ -100,17 +94,16 @@ app.post('/order', async (req, res) => {
       };
       axios.request(createItemOptions)
         .then(res2 => {
-            console.log("Створено новий айтем:", res2.data);
-            res.status(200).json({ message: "Айтем успішно створено" });
+            // res.status(200).json({ message: "Айтем успішно створено" });
           })
         .catch(err => {
-          console.error(err);
+          // console.error(err);
           res.end();
         });
     }
   } catch (error) {
-    console.error('Помилка при зверненні до Webflow API:', error.response?.data || error.message);
-    return res.status(500).json({ error: 'Помилка сервера при пошуку номера' });
+    // console.error('Помилка при зверненні до Webflow API:', error.response?.data || error.message);
+    // return res.status(500).json({ error: 'Помилка сервера при пошуку номера' });
   }
 });
 
