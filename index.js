fb255ea07a901773f62463baeb7cc78827a29f6f
Indiacash23 <sadsad11gg@gmail.com>
2025-06-04 02:33:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 import express from "express";
 import axios from "axios";
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT;
 const app = express();
-app.use(
-express.json({
-  verify: (req, res, buf) => {
-    req.rawBody = buf;
-  }
-})
-);
+app.use(express.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://instacash-india.webflow.io");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
