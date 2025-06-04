32b5816070ccf9c98f2eb07b0f2a57cbdad4d3d0
Indiacash23 <sadsad11gg@gmail.com>
2025-06-04 10:07:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,169 @@ app.use(function (req, res, next) {
 const token = '5d5ea34aa57a25caf974a351dbfd275538dcbbd2277a94b839d8e67e164f34cd';
 const collectionId = '681e1e483b0faaf2ec1eeea7';
 
+app.post('/order', async (req, res) => {
+  const formData = req.body;
+  const phoneFull = formData.Phone_full.replace(/\s+/g, '');
+  const headers = {
+    accept: 'application/json',
+    authorization: `Bearer ${token}`,
+  };
 
+  const limit = 100;
+  let offset = 0;
+  let allItems = [];
+
+  function generate8DigitNumber() {
+    return Math.floor(10000000 + Math.random() * 90000000).toString();
+  }
+
+  async function generateUnique8DigitNumber(existingItems) {
+    let uniqueNumber;
+    let isUnique = false;
+    while (!isUnique) {
+      uniqueNumber = generate8DigitNumber();
+      isUnique = !existingItems.some(
+        item => item.fieldData["unique-id"] === uniqueNumber
+      );
+    }
+    return uniqueNumber;
+  }
+
+  try {
+    while (true) {
+      const response = await axios.get(
+        `https://api.webflow.com/v2/collections/${collectionId}/items?limit=${limit}&offset=${offset}`,
+        { headers }
+      );
+      const items = response.data.items || [];
+      allItems = allItems.concat(items);
+      if (items.length < limit) break;
+      offset += limit;
+    }
+    const foundItem = allItems.find(item => item.fieldData.name.replace(/\s+/g, '') === phoneFull);
+
+    if (foundItem) {
+      const originalSum = parseFloat(foundItem.fieldData.sum || 0);
+      const newSum = parseFloat(formData["Sum"] || 0);
+      let currentSum = originalSum + newSum;
+      if (currentSum > 100000) currentSum = 100000;
+
+      const updateItemOptions = {
+        method: 'PATCH',
+        url: `https://api.webflow.com/v2/collections/${collectionId}/items/${foundItem.id}/live`,
+        headers: {
+          accept: 'application/json',
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${token}`,
+        },
+        data: {
+          isArchived: false,
+          isDraft: false,
+          fieldData: {
+            name: foundItem.fieldData.name,
+            slug: foundItem.fieldData.slug,
+            sum: currentSum,
+          },
+        },
+      };
+
+      const updateResponse = await axios.request(updateItemOptions);
+
+      return res.status(200).json({
+        updatedSum: currentSum,
+      });
+    } else {
+      const uniqueId = await generateUnique8DigitNumber(allItems);
+      const createItemOptions = {
+        method: 'POST',
+        url: `https://api.webflow.com/v2/collections/${collectionId}/items/live`,
+        headers: {
+          accept: 'application/json',
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${token}`,
+        },
+        data: {
+          isArchived: false,
+          isDraft: false,
+          fieldData: {
+            name: phoneFull,
+            slug: phoneFull.replace(/\+/g, ''),
+            "full-name": formData["First-Name"] + ' ' + formData["Last-Name"],
+            city: formData["City"],
+            language: formData["Language"],
+            sum: formData["Sum"],
+            "the-request-has-been-processed": false,
+            messenger: formData["Messenger"],
+            "phone-number-or-nickname-in-messenger": formData["Nick"],
+            status: "61da663c1046e1c2a962dd15679ce3b1",
+            "customer-id": uniqueId,
+            period: formData["Period"],
+          },
+        },
+      };
+
+      axios.request(createItemOptions)
+        .then(() => {
+          res.status(200).json({ customerId: uniqueId });
+        })
+        .catch(err => {
+          console.error('Помилка створення айтема:', err.response?.data || err.message);
+          res.status(500).json({ error: "Помилка створення айтема" });
+        });
+    }
+  } catch (error) {
+    console.error('Помилка при зверненні до Webflow API:', error.response?.data || error.message);
+    return res.status(500).json({ error: 'Помилка сервера при пошуку номера' });
+  }
+});
+
+
+app.post('/log-in', async (req, res) => {
+  const formData = req.body;
+  const phoneFull = formData.Phone_full.replace(/\s+/g, '');
+
+  const headers = {
+    accept: 'application/json',
+    authorization: `Bearer ${token}`,
+  };
+
+  const limit = 100;
+  let offset = 0;
+  let allItems = [];
+
+  try {
+    while (true) {
+      const response = await axios.get(
+        `https://api.webflow.com/v2/collections/${collectionId}/items?limit=${limit}&offset=${offset}`,
+        { headers }
+      );
+      const items = response.data.items || [];
+      allItems = allItems.concat(items);
+      if (items.length < limit) break;
+      offset += limit;
+    }
+    const foundItem = allItems.find(item => item.fieldData.name.replace(/\s+/g, '') === phoneFull);
+
+    if (foundItem) {
+      return res.status(200).json({
+        success: true,
+        user: foundItem.fieldData['full-name'],
+        sum: foundItem.fieldData.sum,
+        status: foundItem.fieldData.status,
+        date: foundItem.createdOn,
+        customerId: foundItem.fieldData['customer-id'],
+      });
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: 'Користувача не знайдено'
+      });
+    }
+  } catch (error) {
+    console.error('Помилка при зверненні до Webflow API:', error.response?.data || error.message);
+    return res.status(500).json({ error: 'Помилка сервера при пошуку номера' });
+  }
+});
 
 
 app.post('/save', async (req, res) => {
@@ -77,7 +239,7 @@ app.post('/save', async (req, res) => {
   }
 });
 
-app.post('/m-delete', async (req, res) => {
+app.post('/delete', async (req, res) => {
   const formData = req.body;
   const phoneFull = formData.phone.replace(/\s+/g, '');
 
